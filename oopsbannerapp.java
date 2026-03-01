4e701a6dc0780583b0208fcd3ea8ce7b9a68849f
km8553-user <km8553@srmist.edu.in>
2026-03-01 16:53:23 +0530

    UC2 - Render OOPS as Banner using print statements

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,17 +1,12 @@
-/**
- * OOPSBannerApp
- * 
- * Prints the literal text "OOPS" to the console.
- * 
- * @author KAPIL M
- * @version 1.0
- */
 class OOPSBannerApp {
-
-    /**
-     * Main method - Entry point of the application
-     */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+        System.out.println("*       *" + "*       *" + " *     * " + " *       ");
+        System.out.println("*       *" + "*       *" + " ******  " + "  *****  ");
+        System.out.println("*       *" + "*       *" + " *       " + "       * ");
+        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
     }
 }
